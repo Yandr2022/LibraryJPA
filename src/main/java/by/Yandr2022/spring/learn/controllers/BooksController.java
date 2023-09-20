39f84aadb509685d9191c6bcfad1a8102ae894ec
Yandr <102684712+Yandr2022@users.noreply.github.com>
2023-09-20 12:06:14 +0300

    refactoring all logic for Book entity - transition to using spring date jpa

--- src/main/java/by/Yandr2022/spring/learn/controllers/BooksController.java
+++ src/main/java/by/Yandr2022/spring/learn/controllers/BooksController.java
@@ -1,9 +1,10 @@
 package by.Yandr2022.spring.learn.controllers;
 
-import by.Yandr2022.spring.learn.dao.BookDAO;
-import by.Yandr2022.spring.learn.dao.PersonDAO;
+
 import by.Yandr2022.spring.learn.models.Book;
 import by.Yandr2022.spring.learn.models.Person;
+import by.Yandr2022.spring.learn.services.BookService;
+import by.Yandr2022.spring.learn.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
@@ -16,84 +17,93 @@ import java.util.Optional;
 @Component
 @RequestMapping("/books")
 public class BooksController {
-//    private final BookDAO bookDAO;
-//    private final PersonDAO personDAO;
-//
-//    @Autowired
-//    public BooksController(BookDAO bookDAO, PersonDAO personDAO) {
-//        this.bookDAO = bookDAO;
-//        this.personDAO = personDAO;
-//    }
-//
-//    @GetMapping()
-//    public String index(Model model) {
-//        model.addAttribute("books", bookDAO.index());
-//        return "books/index";
-//    }
-//
-//    @GetMapping("/{id}")
-//    public String show(@PathVariable("id") int id, Model model, @ModelAttribute("person") Person person) {
-//        model.addAttribute("book", bookDAO.show(id));
-//
-//        Optional<Person> reader = bookDAO.getBookReader(id);
-//
-//        if (reader.isPresent()) {
-//            model.addAttribute("reader", reader.get());
-//        } else {
-//            model.addAttribute("people", personDAO.index());
-//        }
-//
-//        return "books/show";
-//    }
-//
-//    @GetMapping("/new")
-//    public String newBook(@ModelAttribute("book") Book book) {
-//        return "books/new";
-//    }
-//
-//    @PostMapping()
-//    public String create(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult) {
-//
-//        if (bindingResult.hasErrors())
-//            return "books/new";
-//
-//        bookDAO.save(book);
-//        return "redirect:/books";
-//    }
-//
-//    @GetMapping("/{id}/edit")
-//    public String edit(Model model, @PathVariable("id") int id) {
-//        model.addAttribute("book", bookDAO.show(id));
-//        return "books/edit";
-//    }
-//
-//    @PatchMapping("/{id}")
-//    public String update(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult
-//            , @PathVariable("id") int id) {
-//
-//        if (bindingResult.hasErrors())
-//            return "books/edit";
-//
-//        bookDAO.update(id, book);
-//        return "redirect:/books";
-//    }
-//
-//
-//    @DeleteMapping("/{id}")
-//    public String delete(@PathVariable("id") int id) {
-//        bookDAO.delete(id);
-//        return "redirect:/books";
-//    }
-//
-//    @PatchMapping("/{id}/release")
-//    public String release(@PathVariable("id") int id) {
-//        bookDAO.release(id);
-//        return "redirect:/books/" + id;
-//    }
-//
-//    @PatchMapping("/{id}/assign")
-//    public String assign(@PathVariable("id") int id, @ModelAttribute Person selectedReader) {
-//        bookDAO.assign(id, selectedReader);
-//        return "redirect:/books/" + id;
-//    }
+    private final BookService bookService;
+    private final PersonService personService;
+
+
+    @Autowired
+    public BooksController(BookService bookService, PersonService personService) {
+        this.bookService = bookService;
+
+        this.personService = personService;
+    }
+
+    @GetMapping()
+    public String index(Model model, @RequestParam(value = "page", required = false) Integer page,
+                        @RequestParam(value = "per_page", required = false) Integer perPage,
+                        @RequestParam(value = "sort_by_title", required = false) boolean sortByTitle) {
+        if (page == null || perPage == null) {
+            model.addAttribute("books", bookService.findAll(sortByTitle));
+        }else {
+            model.addAttribute("books", bookService.findWithPagination(page, perPage,sortByTitle));
+
+        }
+        return "books/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model, @ModelAttribute("person") Person person) {
+        model.addAttribute("book", bookService.findOneById(id));
+
+        Optional<Person> reader = bookService.getBookReader(id);
+
+        if (reader.isPresent()) {
+            model.addAttribute("reader", reader.get());
+        } else {
+            model.addAttribute("people", personService.findAll(true));
+        }
+
+        return "books/show";
+    }
+
+    @GetMapping("/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "books/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors())
+            return "books/new";
+
+        bookService.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("book", bookService.findOneById(id));
+        return "books/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult
+            , @PathVariable("id") int id) {
+
+        if (bindingResult.hasErrors())
+            return "books/edit";
+
+        bookService.update(id, book);
+        return "redirect:/books";
+    }
+
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        bookService.delete(id);
+        return "redirect:/books";
+    }
+
+    @PatchMapping("/{id}/release")
+    public String release(@PathVariable("id") int id) {
+        bookService.release(id);
+        return "redirect:/books/" + id;
+    }
+
+    @PatchMapping("/{id}/assign")
+    public String assign(@PathVariable("id") int id, @ModelAttribute Person selectedReader) {
+        bookService.assign(id, selectedReader);
+        return "redirect:/books/" + id;
+    }
 }
--- src/main/java/by/Yandr2022/spring/learn/services/BookService.java
+++ src/main/java/by/Yandr2022/spring/learn/services/BookService.java
@@ -4,10 +4,13 @@ import by.Yandr2022.spring.learn.models.Book;
 import by.Yandr2022.spring.learn.models.Person;
 import by.Yandr2022.spring.learn.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -23,7 +26,48 @@ public class BookService {
         return bookRepository.findByReader(reader);
     }
 
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public List<Book> findAll(boolean sortByTitle) {
+        return sortByTitle ? bookRepository.findAll(Sort.by("title")) : bookRepository.findAll();
+    }
+
+    public List<Book> findWithPagination(Integer page, Integer perPage, boolean sortByTitle) {
+        return sortByTitle
+                ? bookRepository.findAll(PageRequest.of(page, perPage, Sort.by("title"))).getContent()
+                : bookRepository.findAll(PageRequest.of(page, perPage)).getContent();
+    }
+
+    public Book findOneById(int id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public Optional<Person> getBookReader(int id) {
+        // Eager loading
+        return bookRepository.findById(id).map(Book::getReader);
+    }
+
+    @Transactional
+    public void save(Book book) {
+        bookRepository.save(book);
+    }
+
+    @Transactional
+    public void update(int id, Book updatedBook) {
+        updatedBook.setId(id);
+        bookRepository.save(updatedBook);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        bookRepository.deleteById(id);
+    }
+    @Transactional
+    public void release(int id) {
+        bookRepository.findById(id).ifPresent(
+                book -> book.setReader(null));
+    }
+    @Transactional
+    public void assign(int id, Person selectedReader) {
+        bookRepository.findById(id).ifPresent(
+                book -> book.setReader(selectedReader));
     }
 }
